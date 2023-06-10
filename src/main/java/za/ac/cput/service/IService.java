2209d6eb806fb9809c6c5e8ddf957e0978942ffe
Tsireledzo Makhado <101458558+Tsireledzo2@users.noreply.github.com>
2023-06-11 01:37:32 +0200

    Update IService.java

--- src/main/java/za/ac/cput/service/IService.java
+++ src/main/java/za/ac/cput/service/IService.java
@@ -1,9 +1,6 @@
 package za.ac.cput.service;
 
 public interface IService <objectType,primaryKey>{
-
-public interface IService <objectType,primaryKey> {
-
     objectType create(objectType objectType);
     objectType read(primaryKey primaryKey);
     objectType update(objectType objectType);
