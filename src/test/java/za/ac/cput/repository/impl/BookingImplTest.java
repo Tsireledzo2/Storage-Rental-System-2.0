17a78804fe19b446c273d10f6d14fe65c9a320ed
Argus <argushakizimana@gmail.com>
2023-06-11 01:29:09 +1000

    fixed

--- src/test/java/za/ac/cput/repository/impl/BookingImplTest.java
+++ src/test/java/za/ac/cput/repository/impl/BookingImplTest.java
@@ -1,13 +1,8 @@
 package za.ac.cput.repository.impl;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import za.ac.cput.domain.Booking;
 import za.ac.cput.factory.BookingFactory;
-
-import java.time.LocalDate;
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,17 +10,18 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class BookingImplTest {
 
-    private static BookingImpl repository = BookingImpl.getRepository();
+    private static final BookingImpl repository = BookingImpl.getRepository();
 
-    // Date date = new Date();
+    // final push to test
 
-    private static Booking booking = BookingFactory.createBooking(new Date(2023, 3, 8),new Date(2023, 12, 8),new Date(2023, 6, 8),true,25);
+    private static final Booking booking = BookingFactory.createBooking(new Date(2023, 3, 8),new Date(2023, 12, 8),new Date(2023, 6, 8),true,25);
 
     @Test
     @Order(1)
     void create() {
         Booking created = repository.create(booking);
         assertNotNull(created);
+        assert booking != null;
         assertEquals(booking.getBookingNumber() , created.getBookingNumber());
         System.out.println("Create" + created);
     }
@@ -33,6 +29,7 @@ class BookingImplTest {
     @Test
     @Order(2)
     void read() {
+        assert booking != null;
         Booking reading = repository.read(booking.getBookingNumber());
         assertNotNull(reading);
         System.out.println("Read"+ reading);
@@ -54,6 +51,7 @@ class BookingImplTest {
     @Test
     @Order(5)
     void delete() {
+        assert booking != null;
         boolean deleted = repository.delete(booking.getBookingNumber());
         assertTrue(deleted);
         System.out.println("Deleted" + deleted);
