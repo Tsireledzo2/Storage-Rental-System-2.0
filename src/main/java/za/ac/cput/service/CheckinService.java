e00a7198d9a710f4e29279382f94a991b2e8e86d
Argus <argushakizimana@gmail.com>
2023-06-11 02:09:49 +1000

    Revert "Service Added"
    This reverts commit 44ef8ae5bf2d8d224b18b196a00d47e1e6098a5c.

--- src/main/java/za/ac/cput/service/CheckinService.java
+++
@@ -1,9 +0,0 @@
-package za.ac.cput.service;
-
-import za.ac.cput.domain.Checkin;
-
-import java.util.Set;
-
-public interface CheckinService extends IService<Checkin,String>{
-    Set<Checkin> getAll();
-}
--- src/main/java/za/ac/cput/service/CheckoutService.java
+++
@@ -1,9 +0,0 @@
-package za.ac.cput.service;
-import za.ac.cput.domain.Checkout;
-
-import java.util.Set;
-
-public interface CheckoutService extends IService<Checkout, String>{
-
-    Set<Checkout> getAll();
-}
