6a1452300a6950f40f80021efb74afe6db57db1b
MukunaChristian <you@example.com>
2023-08-03 23:08:54 +0200

    new factory

--- src/main/java/za/ac/cput/factory/InvoiceLineFactory.java
+++ src/main/java/za/ac/cput/factory/InvoiceLineFactory.java
@@ -10,7 +10,7 @@ import za.ac.cput.domain.InvoiceLine;
 
 public class InvoiceLineFactory {
 
-    public static InvoiceLine buildInvoiceLine(String invoiceLineId,String serviceDescription,int quantity,double unitPrice,double lineTotalAmount ){
+    public static InvoiceLine buildInvoiceLine(String invoiceLineId, String serviceDescription,int quantity,double unitPrice,double lineTotalAmount ){
 
         InvoiceLine invoiceLine = new InvoiceLine.Builder()
                 .setInvoiceLineId(invoiceLineId)
