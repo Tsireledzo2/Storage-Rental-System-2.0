2a4919675e4cbe5161d1cb54d88a98e75f227a4c
DELL <220128871@mycput.ac.za>
2023-08-06 15:47:42 +0200

    Annotations for the StorageUnitTypeService

--- src/main/java/za/ac/cput/domain/StorageUnitType.java
+++ src/main/java/za/ac/cput/domain/StorageUnitType.java
@@ -40,24 +40,48 @@ public class StorageUnitType implements Serializable {
 
     }
 
+    public double getLength() {
+        return length;
+    }
+
     public void setLength(double length) {
         this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
     }
 
     public void setWidth(double width) {
         this.width = width;
     }
 
+    public double getHeight() {
+        return height;
+    }
+
     public void setHeight(double height) {
         this.height = height;
+    }
+
+    public int getUnitSize() {
+        return unitSize;
     }
 
     public void setUnitSize(int unitSize) {
         this.unitSize = unitSize;
     }
 
+    public double getPrice() {
+        return price;
+    }
+
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public StorageUnitType getStorageUnitType() {
+        return storageUnitType;
     }
 
     public void setStorageUnitType(StorageUnitType storageUnitType) {
