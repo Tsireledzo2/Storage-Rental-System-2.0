a67979746feaf7094bec33c71a6525a6bbb1b915
Tsireledzo Makhado <makhtsire29@gmail.com>
2023-08-22 01:28:36 +0200

    Refactoring the vehicle repo and service

--- src/main/java/za/ac/cput/service/impl/VehicleService.java
+++ src/main/java/za/ac/cput/service/impl/VehicleService.java
@@ -7,12 +7,13 @@
 package za.ac.cput.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import za.ac.cput.domain.Vehicle;
 import za.ac.cput.repository.IVehicleRepository;
 import za.ac.cput.service.IVehicleService;
 
-import java.util.Set;
-
+import java.util.List;
+@Service
 public class VehicleService implements IVehicleService {
     private static IVehicleRepository vehicleRepository;
 
@@ -23,26 +24,32 @@ public class VehicleService implements IVehicleService {
 
     @Override
     public Vehicle create(Vehicle vehicle) {
-        return null;
+        return this.vehicleRepository.save(vehicle);
     }
 
     @Override
-    public Vehicle read(String s) {
-        return null;
+    public Vehicle read(String numberPlate) {
+        return this.vehicleRepository.findById(numberPlate).orElse(null);
     }
 
     @Override
     public Vehicle update(Vehicle vehicle) {
+        if(this.vehicleRepository.existsById(vehicle.getNumberPlate()))
+            return this.vehicleRepository.save(vehicle);
         return null;
     }
 
     @Override
-    public boolean delete(String s) {
+    public boolean delete(String numberPlate) {
+        if(this.vehicleRepository.existsById(numberPlate)) {
+            this.vehicleRepository.deleteById(numberPlate);
+            return true;
+        }
         return false;
     }
 
     @Override
-    public Set<Vehicle> getAll() {
-        return null;
+    public List<Vehicle> getAll() {
+        return this.vehicleRepository.findAll();
     }
 }
