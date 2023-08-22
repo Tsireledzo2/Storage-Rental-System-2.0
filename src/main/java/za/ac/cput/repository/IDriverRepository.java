47d52715259e38b17a2ad3656b6b0d1de2e6a4db
Tsireledzo Makhado <makhtsire29@gmail.com>
2023-08-22 02:49:38 +0200

    Refactor code

--- src/main/java/za/ac/cput/repository/IDriverRepository.java
+++ src/main/java/za/ac/cput/repository/IDriverRepository.java
@@ -7,10 +7,11 @@
 package za.ac.cput.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.Driver;
 
 import java.util.List;
-
+@Repository
 public interface IDriverRepository extends JpaRepository<Driver,String> {
     public List<Driver> getAll();
 }
