a2a340df6aef1f0c1b58d294bd5e0e194fdd5e7c
Your Name <you@example.com>
2023-08-28 16:53:31 +0200

    Removed unwanted classes

--- src/main/java/za/ac/cput/repository/IInvoiceLineRepository.java
+++
@@ -1,9 +0,0 @@
-package za.ac.cput.repository;
-
-import za.ac.cput.domain.InvoiceLine;
-
-import java.util.Set;
-
-public interface IInvoiceLineRepository extends IRepository<InvoiceLine, String> {
-    Set<InvoiceLine> getAll();
-}
--- src/main/java/za/ac/cput/repository/IInvoiceRepository.java
+++
@@ -1,9 +0,0 @@
-package za.ac.cput.repository;
-
-import za.ac.cput.domain.Invoice;
-
-import java.util.Set;
-
-public interface IInvoiceRepository extends IRepository<Invoice, String>{
-    public Set<Invoice> getAll();
-}
