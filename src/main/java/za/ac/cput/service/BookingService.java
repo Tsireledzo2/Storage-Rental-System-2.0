10e74d4e6e56d10bf07016cc2cbeb7dff2d9044f
Argus <argushakizimana@gmail.com>
2023-08-31 12:02:56 +0200

    to delete

---
+++ src/main/java/za/ac/cput/service/BookingService.java
@@ -0,0 +1,20 @@
+package za.ac.cput.service;
+/*
+ * BookingService.java
+ * BookingService Entity
+ * @author: Argus Hakizimana Mbogo (220073260)
+ * Date: 09 June 2023
+ */
+
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.Booking;
+
+import java.util.Set;
+
+public interface BookingService extends IService <Booking, String> {
+    Booking create(Booking booking);
+
+    boolean delete(String id);
+
+    Set<Booking> getAll();
+}
---
+++ src/main/java/za/ac/cput/util/Helper.java
@@ -0,0 +1,80 @@
+
+/**
+ * This is a basecode
+ * Helper.java
+ * @author: Tsireledzo Wisdom Makhado (221116273)
+ * Date: 06 June 2023
+ */
+package za.ac.cput.util;
+
+import org.apache.commons.validator.routines.EmailValidator;
+
+
+import java.time.LocalDate;
+import java.util.Date;
+
+import java.util.List;
+import java.util.UUID;
+//ar
+public class Helper {
+
+    public static Boolean isNullorEmpty(LocalDate s){
+        if(s==null){
+            return true;
+        }
+        return false;
+    }
+
+
+    public static Boolean isNullorEmpty(String s){
+        if(s==null||s.isBlank()){
+
+            return true;
+        }
+        return false;
+    }
+
+    public static Boolean isNullorEmpty2(List s){
+        if(s==null||s.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+
+    public static Boolean isNullorEmpty3(Double s){
+
+        if(s==null||s.isNaN()){
+            return true;
+        }
+        return false;
+    }
+
+    //auto generate id
+    public static String generateId(){
+        return UUID.randomUUID().toString();
+    }
+
+    public static String generateNumber(){
+        float min = 100000000000L;
+        long max = 260000000000L;
+        long studentNumber = (long)(Math.random()*(max-min+1)+min);
+        return String.valueOf(studentNumber);
+    }
+
+
+
+    //validate the email
+    public static Boolean isValidEmail(String email){
+
+
+        EmailValidator emailValidator = EmailValidator.getInstance();
+        return emailValidator.isValid(email);
+
+    }
+
+
+
+
+}
+
+
