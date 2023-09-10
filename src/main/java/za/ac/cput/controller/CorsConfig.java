8100b150392ed49d0e3c32b6429c83d61f01e173
MukunaChristian <you@example.com>
2023-09-10 23:31:03 +0200

    email class

---
+++ src/main/java/za/ac/cput/controller/CorsConfig.java
@@ -0,0 +1,18 @@
+package za.ac.cput.controller;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200") // Add the origin of your Angular application
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+}
