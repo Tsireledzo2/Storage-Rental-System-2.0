6cec24b29fd9e61af6e8bea69e7fcfcf18da1698
Your Name <you@example.com>
2023-09-15 01:37:19 +0200

    Added EmployeeFactoryTest tests

---
+++ src/test/java/za/ac/cput/factory/EmployeeFactoryTest.java
@@ -0,0 +1,15 @@
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.Employee;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeFactoryTest {
+    @Test
+    void buildEmployee(){
+        Employee employee = EmployeeFactory.buildEmployee("Lithemba","Nkqayi","lithe@gmail.com","Nkqa$15");
+        System.out.println("Employee:" +employee);
+        assertNotNull(employee.getEmployeeNumber());
+    }
+}
